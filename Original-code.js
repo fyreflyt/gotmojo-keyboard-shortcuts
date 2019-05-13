ac9ef58d68a0a7aa290094cc37cc25cb008599f8
fyreflyt <fyreflyt@hotmail.com>
2019-05-13 16:09:54 -0400

    Create Original-code.js

---
+++ Original-code.js
@@ -0,0 +1,15 @@
+$(document).keyup(function(e) { 
+    if (e.keyCode === 27) { 
+        $(".a-modal-item__close").click();
+    } 
+});
+$(window).bind('keydown', function(event) {
+    if (event.ctrlKey || event.metaKey) {
+        switch (String.fromCharCode(event.which).toLowerCase()) {
+        case 's':
+            event.preventDefault();
+            $(".a-button--save").click();
+            break;
+        }
+    }
+});
