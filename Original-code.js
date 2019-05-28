7cbb719aef817c30aad4cf4913dbf9cd98cbc477
fyreflyt <fyreflyt@hotmail.com>
2019-05-28 16:42:13 -0400

    Updated the Media Library click
    The Media Library modal now displays images in newest to oldest order via flex's wrap-reverse attribute.

--- Original-code.js
+++ Original-code.js
@@ -1,16 +1,23 @@
-$(document).keyup(function(e) { 
-    if (e.keyCode === 27) { 
-        $(".a-modal-item__close").click();
-    } 
+$(document).keyup(function(e) {
+  if (e.keyCode === 27) {
+    $(".a-modal-item__close").click();
+  }
 });
-$(window).bind('keydown', function(event) {
-    if (event.ctrlKey || event.metaKey) {
-        switch (String.fromCharCode(event.which).toLowerCase()) {
-        case 's':
-            event.preventDefault();
-            $(".a-button--save").click();
-            break;
-        }
+$(window).bind("keydown", function(event) {
+  if (event.ctrlKey || event.metaKey) {
+    switch (String.fromCharCode(event.which).toLowerCase()) {
+      case "s":
+        event.preventDefault();
+        $(".a-button--save").click();
+        break;
     }
+  }
 });
-jQuery(".resource-items-container").css('flex-flow','row wrap-reverse');
+
+jQuery(".a-icon-images")
+  .closest("button")
+  .bind("click", function() {
+    setTimeout(function() {
+      jQuery(".resource-items-container").css("flex-flow", "row wrap-reverse");
+    }, 50);
+  });
